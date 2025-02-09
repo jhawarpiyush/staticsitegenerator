0f77ee6c9fa2879c41d0395338acd0a9364113c4
Piyush Jhawar <pjhawargithub@gmail.com>
2025-02-08 22:35:42 -0600

    Completed inline markdown split code.

---
+++ src/inline_markdown_split.py
@@ -0,0 +1,85 @@
+import re
+
+
+from textnode import TextType, TextNode
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_list = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            continue
+        split_text = node.text.split(delimiter)
+        if len(split_text) % 2 == 0:
+            raise ValueError("closing markdown delimiter not found.")
+        for i, text in enumerate(split_text):
+            if text:
+                if i % 2 == 0:
+                    new_list.append(TextNode(text, node.text_type))
+                else:
+                    new_list.append(TextNode(text, text_type))
+    return new_list
+
+
+def extract_markdown_images(text):
+    imgaltandsrc = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return imgaltandsrc
+
+
+def extract_markdown_links(text):
+    anchortextandlink = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return anchortextandlink
+
+
+def split_nodes_image(old_nodes):
+    new_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        if len(images) == 0:
+            new_list.append(node)
+            continue
+        for img in images:
+            split_text = node.text.split(f"[{img[0]}]({img[1]})", 1)
+            if len(split_text) != 2:
+                raise ValueError("invalid markdown image not closed")
+            if split_text[0]:
+                new_list.append(TextNode(split_text[0], node.text_type))
+            new_list.append(TextNode(img[0], TextType.IMAGE, img[1]))
+            node.text = split_text[1]
+    return new_list
+
+
+def split_nodes_link(old_nodes):
+    new_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            continue
+        links = extract_markdown_links(node.text)
+        if len(links) == 0:
+            new_list.append(node)
+            continue
+        for link in links:
+            split_text = node.text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(split_text) != 2:
+                raise ValueError("invalid markdown link not closed")
+            if split_text[0]:
+                new_list.append(TextNode(split_text[0], node.text_type))
+            new_list.append(TextNode(link[0], TextType.LINK, link[1]))
+            node.text = split_text[1]
+    return new_list
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from inline_markdown_split import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_link, text_to_textnodes
 from textnode import TextNode, text_node_to_html_node, TextType
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    # node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     # htmlnode = HTMLNode(props={"href": "https://www.google.com","target": "_blank"})
     # print(htmlnode.props_to_html())
 
     # node = ParentNode("p",[LeafNode("b", "Bold text"),LeafNode(None, "Normal text"),LeafNode("i", "italic text"),LeafNode(None, "Normal text"),LeafNode("a", "ddf", "{'a':'b'}"),],)
     # print(node.to_html())
-    node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
-    print(text_node_to_html_node(node))
+    # node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+    # print(text_node_to_html_node(node))
+
+    # node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    # new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    # print(new_nodes)
+    
+    # text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    # extract_markdown_images(text)
+
+    # text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    # print(extract_markdown_links(text))
+
+    # node = TextNode(
+    # "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    # TextType.TEXT,)
+    # new_nodes = split_nodes_link([node])
+
+    text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
 
 
 if __name__ == "__main__":
